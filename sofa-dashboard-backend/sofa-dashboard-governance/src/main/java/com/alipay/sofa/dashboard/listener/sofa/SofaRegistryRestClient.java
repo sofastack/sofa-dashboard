091082ea166c88fbfdda7664de3148d57bb4a22b
卫恒 <guolei.sgl@antfin.com>
2019-05-07 15:20:33 +0800

    provide data refresh when request coming

--- sofa-dashboard-backend/sofa-dashboard-governance/src/main/java/com/alipay/sofa/dashboard/listener/sofa/SofaRegistryRestClient.java
+++ sofa-dashboard-backend/sofa-dashboard-governance/src/main/java/com/alipay/sofa/dashboard/listener/sofa/SofaRegistryRestClient.java
@@ -30,8 +30,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
