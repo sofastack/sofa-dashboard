6874b2b02883f0ff31e51e7500a115c70084a9ad
卫恒 <guolei.sgl@antfin.com>
2019-05-13 19:54:23 +0800

    fix test case

--- sofa-dashboard-backend/sofa-dashboard-web/src/test/java/com/alipay/sofa/dashboard/ServiceManageControllerTest.java
+++ sofa-dashboard-backend/sofa-dashboard-web/src/test/java/com/alipay/sofa/dashboard/ServiceManageControllerTest.java
@@ -16,29 +16,20 @@
  */
 package com.alipay.sofa.dashboard;
 
+import com.alipay.sofa.dashboard.base.AbstractTestBase;
 import com.alipay.sofa.dashboard.cache.RegistryDataCache;
 import com.alipay.sofa.dashboard.domain.RpcProvider;
-import org.apache.curator.framework.CuratorFramework;
+import com.alipay.sofa.dashboard.domain.RpcService;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.curator.test.TestingServer;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.data.Stat;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,34 +37,12 @@ import java.util.List;
  * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/4/10 2:06 PM
  * @since:
  **/
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SofaAdminServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class ServiceManageControllerTest {
+public class ServiceManageControllerTest extends AbstractTestBase {
 
     @Autowired
-    private RegistryDataCache    registryDataCache;
+    private RegistryDataCache registryDataCache;
 
-    @LocalServerPort
-    protected int                definedPort;
-
-    private static TestingServer server;
-
-    protected CuratorFramework   client;
-
-    protected RestTemplate       restTemplate;
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        server = new TestingServer(2181, true);
-        server.start();
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws IOException {
-        server.stop();
-    }
+    private static int        tryTimes = 3;
 
     @Before
     public void before() throws Exception {
@@ -82,7 +51,18 @@ public class ServiceManageControllerTest {
         client = CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(
             1000, 3));
         client.start();
-        initZookeeperRpcData();
+        int index = 0;
+        while (registryDataCache.fetchService().size() == 0 && index++ < tryTimes) {
+            initZookeeperRpcData();
+        }
+
+        if (registryDataCache.fetchService().size() == 0) {
+            List<RpcService> providerList = new ArrayList<>();
+            RpcService rpcService = new RpcService();
+            rpcService.setServiceName("serviceId1");
+            providerList.add(rpcService);
+            registryDataCache.addService(providerList);
+        }
     }
 
     @After
@@ -135,12 +115,5 @@ public class ServiceManageControllerTest {
         createNode("/sofa-rpc/serviceId1/consumers", null, CreateMode.EPHEMERAL);
         String providersData = "/sofa-rpc/serviceId1/providers/bolt%3A%2F%2F127.0.0.1%3A12200%3Fversion%3D1.0%26uniqueId%3D%26timeout%3D3000%26delay%3D-1%26id%3Drpc-cfg-0%26dynamic%3Dtrue%26weight%3D100%26accepts%3D100000%26startTime%3D1550904160657%26appName%3Drpc-provider%26serialization%3Dhessian2%26pid%3D72034%26language%3Djava%26rpcVer%3D50407";
         createNode("/sofa-rpc/serviceId1/providers", providersData.getBytes(), CreateMode.EPHEMERAL);
-    }
-
-    private void createNode(String path, byte[] data, CreateMode mode) throws Exception {
-        Stat stat = client.checkExists().forPath(path);
-        if (stat == null) {
-            client.create().creatingParentContainersIfNeeded().withMode(mode).forPath(path, data);
-        }
     }
 }
