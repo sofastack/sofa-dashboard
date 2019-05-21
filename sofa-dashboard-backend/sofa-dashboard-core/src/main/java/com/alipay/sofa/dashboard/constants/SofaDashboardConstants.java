96127b3794bf11c14cc23c51cec933680bd5dd9e
卫恒 <guolei.sgl@antfin.com>
2019-05-21 11:25:19 +0800

    sync master

--- sofa-dashboard-backend/sofa-dashboard-core/src/main/java/com/alipay/sofa/dashboard/constants/SofaDashboardConstants.java
+++ sofa-dashboard-backend/sofa-dashboard-core/src/main/java/com/alipay/sofa/dashboard/constants/SofaDashboardConstants.java
@@ -57,9 +57,9 @@ public class SofaDashboardConstants {
     public static final String Q_MARK                      = "?";
     public static final String SEMICOLON                   = ";";
     public static final String HORIZONTAL                  = "-";
-    public static final String EMPTY                     = "";
-    public static final String LOCALHOST                 = "localhost";
-    public static final String LOCALHOST_IP              = "127.0.0.1";
+    public static final String EMPTY                       = "";
+    public static final String LOCALHOST                   = "localhost";
+    public static final String LOCALHOST_IP                = "127.0.0.1";
 
     public static final String DATA_ID_SEPARATOR           = "#@#";
     public static final String REGISTRY_DATA_ID_KEY        = "dataId";
